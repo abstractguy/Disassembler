742fad09bfe26dd632a496c8dbcbce9d82874544
abstractguy <wiseakasha@hotmail.com>
2017-05-28 17:09:18 -0400

    Update text_management.h

--- text_management.h
+++ text_management.h
@@ -15,14 +15,22 @@
     mode mode;
   } record;
  
+  typedef struct RecordList {
+    record *record;
+    struct RecordList *record_list;
+  } record_list;
+ 
   extern int char_count(char *, char);
   unsigned char ASCII_to_byte(char *);
   char **string_separate(char **, char *, char *);
   extern void destroy_array(char *);
   char **create_strings(char *, int);
   void destroy_strings(char **, int);
-  record *create_record(record *, unsigned char, unsigned short int, mode, unsigned char *, unsigned char);
+  record *create_record(unsigned char, unsigned short int, mode, unsigned char *, unsigned char);
   extern void destroy_record(record *);
-  record *build_record_from_string(record *, char *);
-  record **reconstruct_records(char *, int);
+  extern record_list *destroy_record_list(record_list *);
+  record *build_record_from_string(char *);
+  record_list *push_record(record *, record_list *);
+  extern record_list *hex_file_to_record_list(char *);
+  //record **reconstruct_records(char *, int);
 #endif
