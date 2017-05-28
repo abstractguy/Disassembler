aa38355563e95c71d3c767569570392e6e2b583b
abstractguy <wiseakasha@hotmail.com>
2017-05-28 17:06:59 -0400

    Update test.c

--- test.c
+++ test.c
@@ -3,39 +3,34 @@
 #include "text_management.h"
  
 int main(int argc, char *argv[]) {
-  int i, size;
-  char *array = NULL;
-  record **records = NULL;
+  record_list *_record_list = NULL;
+  record *record = NULL;
  
   assert(argc == 2);
-  array = file_to_array(array, argv[1]);
-  size = char_count(array, ':');
+
+  _record_list = hex_file_to_record_list(argv[1]);
+
+  do {
+    record = _record_list->record;
+    printf("size: %u, address: 0x%4.4X, ", record->size, record->address);
  
-  records = reconstruct_records(array, size);
- 
-  for (i = 0; i < size; i++) {
-    printf("size: %u, address: 0x%4.4X, ", records[i]->size, records[i]->address);
- 
-    switch (records[i]->mode) {
+    switch (record->mode) {
       case DATA: printf("record mode: DATA"); break;
       case END: printf("record mode: END");   break;
       case EXTENDED: printf("record mode: EXTENDED");
     }
  
-    if (records[i]->size) {
+    if (record->size) {
       printf("\nbytecode: ");
-      for (int j = 0; j < records[i]->size; j++) printf("%.2X", records[i]->bytecode[j]);
+      for (int j = 0; j < record->size; j++) printf("%.2X", record->bytecode[j]);
     }
- 
-    printf("\nchecksum: 0x%X\n\n", records[i]->checksum);
- 
-    destroy_record(records[i]);
-  }
- 
+
+    printf("\nchecksum: 0x%X\n\n", record->checksum);
+
+    destroy_record(record);
+  } while ((_record_list = destroy_record_list(_record_list)));
+
   printf("\n\n");
- 
-  destroy_array(array);
-  free(records);
- 
+
   return 0;
 }
