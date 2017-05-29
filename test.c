af96533883da5ef54308402df09ad785a20ce463
abstractguy <wiseakasha@hotmail.com>
2017-05-29 11:34:07 -0400

    Update test.c
    Complete rewrite

--- test.c
+++ test.c
@@ -1,36 +1,14 @@
 // test.c
- 
-#include "text_management.h"
- 
+
+#include "record_management.h"
+
 int main(int argc, char *argv[]) {
-  record_list *_record_list = NULL;
   record *record = NULL;
- 
+
   assert(argc == 2);
+  record = hex_file_to_records(argv[1]);
 
-  _record_list = hex_file_to_record_list(argv[1]);
-
-  do {
-    record = _record_list->record;
-    printf("size: %u, address: 0x%4.4X, ", record->size, record->address);
- 
-    switch (record->mode) {
-      case DATA: printf("record mode: DATA"); break;
-      case END: printf("record mode: END");   break;
-      case EXTENDED: printf("record mode: EXTENDED");
-    }
- 
-    if (record->size) {
-      printf("\nbytecode: ");
-      for (int j = 0; j < record->size; j++) printf("%.2X", record->bytecode[j]);
-    }
-
-    printf("\nchecksum: 0x%X\n\n", record->checksum);
-
-    destroy_record(record);
-  } while ((_record_list = destroy_record_list(_record_list)));
-
-  printf("\n\n");
+  do {print_record(record);} while ((record = destroy_record(record)));
 
   return 0;
 }
