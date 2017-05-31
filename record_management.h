9374ca364f4652debc8e5c6809c311154bffb422
abstractguy <wiseakasha@hotmail.com>
2017-05-31 15:51:49 -0400

    Update record_management.h

--- record_management.h
+++ record_management.h
@@ -4,9 +4,7 @@
   #define RECORD_MANAGEMENT_H
   #include "text_management.h"
 
-  typedef enum {
-    DATA, END, EXTENDED
-  } mode;
+  typedef enum {DATA, END, EXTENDED} mode;
  
   typedef struct Record {
     unsigned char size, *bytecode, checksum;
@@ -18,6 +16,8 @@
   record *build_record_from_string(char *, record *);
   record *create_record(unsigned char, unsigned short int, mode, unsigned char *, unsigned char, record *);
   extern record *destroy_record(record *);
+  record *get_last_record(record *);
+  record *replace_record_with_record_block(record *, record *);
   extern record *hex_file_to_records(char *);
   extern void print_record(record *);
 #endif
