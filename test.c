e72e08e54fd046e840ecadba2e16ffa61f13bf50
abstractguy <wiseakasha@hotmail.com>
2017-05-31 15:52:40 -0400

    Update test.c

--- test.c
+++ test.c
@@ -1,12 +1,13 @@
 // test.c
 
-#include "record_management.h"
+#include "instruction_set.h"
 
 int main(int argc, char *argv[]) {
   record *record = NULL;
 
   assert(argc == 2);
   record = hex_file_to_records(argv[1]);
+  //record = unroll_subrecord_addresses(record);
 
   do {print_record(record);} while ((record = destroy_record(record)));
 
