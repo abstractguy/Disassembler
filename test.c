a47cb80af80580ff872074df6a5a87f38b548ca8
abstractguy <wiseakasha@hotmail.com>
2017-06-04 03:16:03 -0400

    Update test.c

--- test.c
+++ test.c
@@ -1,17 +1,21 @@
 // test.c
 
 #include "instruction_set.h"
-
+ 
 int main(int argc, char *argv[]) {
   record *record = NULL;
 
   assert(argc == 2);
+
   record = hex_file_to_records(argv[1]);
 
-  do {
+  record = align_records(record);
+
+  while (record) {
     record = extract_instruction(record);
     print_record(record);
-  } while ((record = destroy_record(record)));
+    record = destroy_record(record);
+  }
 
   return 0;
 }
