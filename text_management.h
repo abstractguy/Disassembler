2e48ec961b4f5b1e71d844c4c120915d7852ff82
abstractguy <wiseakasha@hotmail.com>
2017-06-04 03:17:11 -0400

    Update text_management.h

--- text_management.h
+++ text_management.h
@@ -9,6 +9,9 @@
   unsigned char ASCII_to_byte(char *);
   char **string_separate(char **, char *, char *);
   void destroy_array(char *), destroy_strings(char **, int);
-  unsigned char *copy_bytecode(unsigned char *, int);
+  unsigned char *create_bytevector(unsigned short int);
+  void destroy_bytevector(unsigned char *);
+  unsigned char *copy_bytevector(unsigned char *, unsigned short int);
+  unsigned char *concatenate_bytevectors(unsigned char *, unsigned short int, unsigned char *, unsigned short int);
   char **create_strings(char *, int);
 #endif
