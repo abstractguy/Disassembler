e1c3af1ff4d941be3304201eed3cff1521bb4afb
abstractguy <wiseakasha@hotmail.com>
2017-06-04 03:17:54 -0400

    Update text_management.c

--- text_management.c
+++ text_management.c
@@ -33,23 +33,43 @@ void destroy_strings(char **strings, int size) {
     free(strings);
   }
 }
- 
-unsigned char *copy_bytecode(unsigned char *old_bytecode, int size) {
-  unsigned char *new_bytecode = NULL;
- 
-  assert(new_bytecode = (unsigned char *)calloc(size, sizeof(unsigned char)));
- 
-  memcpy(new_bytecode, old_bytecode, sizeof(unsigned char) * size);
- 
-  return new_bytecode;
+
+unsigned char *create_bytevector(unsigned short int size) {
+  unsigned char *bytevector = NULL;
+
+  assert(bytevector = (unsigned char *)calloc(size, sizeof(unsigned char)));
+
+  return bytevector;
 }
- 
+
+void destroy_bytevector(unsigned char *bytevector) {
+  if (bytevector) free(bytevector);
+  bytevector = NULL;
+}
+
+unsigned char *copy_bytevector(unsigned char *old_bytevector, unsigned short int size) {
+  unsigned char *new_bytevector = create_bytevector(size);
+
+  memcpy(new_bytevector, old_bytevector, sizeof(unsigned char) * size);
+
+  return new_bytevector;
+}
+
+unsigned char *concatenate_bytevectors(unsigned char *bytevector1, unsigned short int size1, unsigned char *bytevector2, unsigned short int size2) {
+  unsigned char *bytevector = create_bytevector(size1 + size2);
+
+  memcpy(bytevector, bytevector1, size1);
+  memcpy(&bytevector[size1], bytevector2, size2);
+
+  return bytevector;
+}
+
 char **string_separate(char **strings, char *string, char *delimiters) {
   int i = 1;
   char *token = NULL;
- 
+
   if ((token = strtok(string, delimiters))) strings[0] = strdup(token);
   while ((token = strtok(NULL, delimiters))) strings[i++] = strdup(token);
- 
+
   return strings;
 }
