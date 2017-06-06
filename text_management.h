348f2e00dd883cf661d512e2d7154311342defb5
abstractguy <wiseakasha@hotmail.com>
2017-06-05 22:00:33 -0400

    Update text_management.h

--- text_management.h
+++ text_management.h
@@ -5,13 +5,16 @@
   #include "file_management.h"
   #include <string.h>
 
-  int char_count(char *, char);
-  unsigned char ASCII_to_byte(char *);
-  char **string_separate(char **, char *, char *);
-  void destroy_array(char *), destroy_strings(char **, int);
   unsigned char *create_bytevector(unsigned short int);
   void destroy_bytevector(unsigned char *);
   unsigned char *copy_bytevector(unsigned char *, unsigned short int);
   unsigned char *concatenate_bytevectors(unsigned char *, unsigned short int, unsigned char *, unsigned short int);
-  char **create_strings(char *, int);
+
+  char **create_strings(unsigned short int);
+  void destroy_strings(char **, int);
+  char **string_separate(char **, char *, char *);
+  char **separate_record_strings(char *, int);
+
+  unsigned char ASCII_to_byte(char *);
+  int char_count(char *, char);
 #endif
