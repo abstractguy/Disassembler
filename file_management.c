d712e31f9b22a437d72eb6988262063d706c18e7
abstractguy <wiseakasha@hotmail.com>
2017-06-05 22:05:52 -0400

    Update file_management.c

--- file_management.c
+++ file_management.c
@@ -13,7 +13,7 @@ extern char *file_to_array(char *array, char *file) {
   FILE *fp = NULL;
   int i = 0;
   assert(fp = fopen(file, "r"));
-  assert(array = (char *)calloc(file_size(fp) + 1, sizeof(char)));
+  assert(array = calloc(file_size(fp) + 1, sizeof(char)));
   do {array[i] = fgetc(fp);} while (array[i++] != EOF);
   array[i - 1] = '\0';
   assert(fclose(fp) != EOF);
