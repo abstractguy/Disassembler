8fcba0f4b9f3fcd3b02a178155dc8540bed0cdd3
abstractguy <wiseakasha@hotmail.com>
2017-06-07 15:34:39 -0400

    Update test.c

--- test.c
+++ test.c
@@ -3,9 +3,10 @@
 #include "instruction_set.h"
  
 int main(int argc, char *argv[]) {
-  record *record = NULL;
+  record *record = NULL, *temporary = NULL;
   assert(argc == 2);
   record = extract_instructions(argv[1]);
-  do {print_record(record);} while ((record = destroy_record(record)));
+  while ((print_record(record)));
+  record = destroy_all_records(record);
   return 0;
 }
