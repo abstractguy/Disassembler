e5dac2578ff4ce53422025d47dc358353d2cfb41
abstractguy <wiseakasha@hotmail.com>
2017-06-07 14:04:00 -0400

    Update record_management.h

--- record_management.h
+++ record_management.h
@@ -16,7 +16,7 @@
  
   record *create_record(unsigned short int, unsigned short int, mode, unsigned char *, unsigned char, record *);
   extern record *destroy_record(record *);
-  record *copy_record(record *);
+  record *fork_record(record *, unsigned short int, unsigned short int, unsigned char *, record *);
   record *reverse_records(record *);
   record *build_record_from_string(char *, record *);
   extern record *hex_file_to_records(char *);
