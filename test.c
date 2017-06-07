1314884f0d8b8cac4850487c5506f4063400153a
abstractguy <wiseakasha@hotmail.com>
2017-06-07 12:37:21 -0400

    Update test.c

--- test.c
+++ test.c
@@ -4,15 +4,8 @@
  
 int main(int argc, char *argv[]) {
   record *record = NULL;
-
   assert(argc == 2);
-
-  record = align_records(hex_file_to_records(argv[1]));
-
-  do {
-    record = extract_instruction(record);
-    print_record(record);
-  } while ((record = destroy_record(record)));
-
+  record = extract_instructions(argv[1]);
+  do {print_record(record);} while ((record = destroy_record(record)));
   return 0;
 }
