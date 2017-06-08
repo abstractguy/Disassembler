5bcd421f409f3f4ea966ea3d8d4f60535b9c2bb7
abstractguy <wiseakasha@hotmail.com>
2017-06-07 20:54:49 -0400

    Update record_management.c

--- record_management.c
+++ record_management.c
@@ -30,84 +30,87 @@ record *destroy_record(record *old_record) {
 record *copy_record_from_offset(record *records, unsigned short int size, unsigned short int offset, record *next) {
   return create_record(size, records->address + offset, records->mode, &records->bytecode[offset], records->checksum, next);
 }
- 
+
 record *reverse_records(record *forward) {
-  record *backward = NULL;
+  record *backward = NULL, *next = NULL;
  
   while (forward) {
-    backward = create_record(forward->size, forward->address, forward->mode, forward->bytecode, forward->checksum, backward);
-    forward = destroy_record(forward);
+    next            = forward->record;
+    forward->record = backward;
+    backward        = forward;
+    forward         = next;
   }
  
   return backward;
 }
- 
-record *build_record_from_string(char *string, record *next) {
-  record *record = NULL;
-  unsigned short int size = strlen(string) / 2;
-  unsigned char *bytevector = NULL;
- 
-  bytevector = create_bytevector(size);
- 
-  for (unsigned short int i = 0; i < size; i++) bytevector[i] = ASCII_to_byte(&string[i * 2]);
- 
-  record = create_record((unsigned short int)bytevector[0], ((unsigned short int) bytevector[1] << 8) + (unsigned short int) bytevector[2], bytevector[3], &bytevector[4], bytevector[bytevector[0] + 4], next);
- 
-  destroy_bytevector(bytevector);
-  return record;
-}
- 
+
 record *align_instructions(record *forward) {
   record *temporary = NULL, *backward = NULL;
   unsigned char *bytecode = NULL;
  
   while (forward != NULL) {
+
     if (forward->record != NULL && (forward->address + forward->size) == forward->record->address) {
+
       bytecode = concatenate_bytevectors(forward->bytecode, forward->size, forward->record->bytecode, forward->record->size);
  
       temporary = create_record(forward->size + forward->record->size, forward->address, forward->mode, bytecode, (unsigned char)(forward->checksum + forward->record->checksum), forward->record->record);
  
       destroy_bytevector(bytecode);
+
       forward = destroy_record(destroy_record(forward));
       forward = temporary;
  
     } else {
  
       if (forward->size) backward = create_record(forward->size, forward->address, forward->mode, forward->bytecode, forward->checksum, backward);
- 
+
       forward = destroy_record(forward);
     }
   }
- 
+
   return reverse_records(backward);
 }
- 
+
 extern record *hex_file_to_records(char *file) {
   record *records = NULL;
-  unsigned short int size, i;
-  char *array = file_to_array(file), **strings = NULL;
- 
-  size = i = char_count(array, ':');
- 
-  assert(size);
-  strings = string_separate(array, "\r\n:");
- 
-  do {records = build_record_from_string(strings[--i], records);} while (i);
+  unsigned short int i, size = 0, substring_size;
+  unsigned char *bytevector = NULL;
+  char *array = NULL, **strings = NULL;
+
+  array          = file_to_array(file);
+  assert(size    = i = char_count(array, ':'));
+  strings        = string_separate(array, "\r\n:");
 
   destroy_bytevector((unsigned char *)array);
+
+  while (i--) {
+
+    substring_size = strlen(strings[i]) / 2;
+    bytevector     = create_bytevector(substring_size);
+
+    for (unsigned short int j = 0; j < substring_size; j++) {
+      bytevector[j] = ASCII_to_byte(&strings[i][j * 2]);
+    }
+
+    records = create_record((unsigned short int)bytevector[0], ((unsigned short int)bytevector[1] << 8) + (unsigned short int)bytevector[2], bytevector[3], &bytevector[4], bytevector[bytevector[0] + 4], records);
+
+    destroy_bytevector(bytevector);
+  }
+
   destroy_strings(strings, size);
- 
+
   return align_instructions(records);
 }
- 
+
 extern record *print_record(record *record) {
   printf("size: %u, address: 0x%4.4X, ", record->size, record->address);
- 
+
   switch (record->mode) {
     case DATA: printf("record mode: DATA"); break;
     case END: printf("record mode: END");
   }
- 
+
   if (record->size) {
     printf("\nbytecode: ");
     for (unsigned short int j = 0; j < record->size; j++)
