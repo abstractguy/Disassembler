ff5873e1ce66cab3eb0b30346975b11eb1547739
abstractguy <wiseakasha@hotmail.com>
2017-06-10 19:16:29 -0400

    Add files via upload

--- string_management.c
+++ string_management.c
@@ -5,17 +5,18 @@
 char **string_separate(char *string, char *delimiters) {
   char *token = NULL, **strings = create_strings(char_count(string, ':'));
 
-  if ((token = strtok(string, delimiters)))
-    for (unsigned short int i = 0; token; i++, token = strtok(NULL, delimiters))
+  if ((token = strtok(string, delimiters))) {
+    for (unsigned short int i = 0; token; i++, token = strtok(NULL, delimiters)) {
       strings[i] = strdup(token);
- 
-  return strings;
+    }
+  } return strings;
 }
  
 unsigned short int char_count(char *string, char character) {
   unsigned short int size = 0;
-  for (unsigned short int i = 0; string[i]; i++) if (string[i] == character) size++;
-  return size;
+  for (unsigned short int i = 0; string[i]; i++) {
+    if (string[i] == character) size++;
+  } return size;
 }
  
 unsigned char ASCII_to_byte(char *ASCII) {
